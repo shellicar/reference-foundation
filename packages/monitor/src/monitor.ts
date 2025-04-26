ea32c8e18f5b18c9203582c6ff98ed4c65261358
Stephen Hellicar <shellicar@gmail.com>
2025-04-26 17:30:13 +1000

    Update packages (#16)
    nuxt @ 3.15.4 due to issue with 3.16
    tailwindcss @ 3.x due to major updates in 4.x

--- packages/monitor/src/monitor.ts
+++ packages/monitor/src/monitor.ts
@@ -1,5 +1,5 @@
 import { type AzureMonitorOpenTelemetryOptions, useAzureMonitor } from '@azure/monitor-opentelemetry';
-import { Resource } from '@opentelemetry/resources';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { ATTR_SERVICE_INSTANCE_ID, ATTR_SERVICE_NAMESPACE } from '@opentelemetry/semantic-conventions/incubating';
 import { TelemetryClient } from 'applicationinsights';
@@ -17,7 +17,7 @@ export type ServiceType = {
 };
 
 export const getMonitor = (config: AzureMonitorConfig = {}) => {
-  const customResource = new Resource({
+  const customResource = resourceFromAttributes({
     [ATTR_SERVICE_NAME]: config.service?.name,
     [ATTR_SERVICE_NAMESPACE]: config.service?.namespace,
     [ATTR_SERVICE_INSTANCE_ID]: config.service?.instanceId,
